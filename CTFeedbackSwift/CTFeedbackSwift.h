1fb046ec45ddf3bb77b2330722e42cd543e2891a
Ryoichi Izumita <r.izumita@lang-8.jp>
2017-09-07 16:46:31 +0900

    Initial Commit

---
+++ CTFeedbackSwift/CTFeedbackSwift.h
@@ -0,0 +1,19 @@
+//
+//  CTFeedbackSwift.h
+//  CTFeedbackSwift
+//
+//  Created by 和泉田 領一 on 2017/09/07.
+//  Copyright © 2017年 CAPH TECH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CTFeedbackSwift.
+FOUNDATION_EXPORT double CTFeedbackSwiftVersionNumber;
+
+//! Project version string for CTFeedbackSwift.
+FOUNDATION_EXPORT const unsigned char CTFeedbackSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTFeedbackSwift/PublicHeader.h>
+
+
