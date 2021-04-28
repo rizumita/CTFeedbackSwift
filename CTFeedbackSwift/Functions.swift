910444640dceb7e8ec1dd79b0f04ed0216874d71
Ryoichi Izumita <r.izumita@caph.jp>
2021-04-29 05:52:14 +0900

    Fix bundle

--- CTFeedbackSwift/Functions.swift
+++ CTFeedbackSwift/Functions.swift
@@ -8,7 +8,12 @@ import MobileCoreServices
 import AVFoundation
 
 func CTLocalizedString(_ key: String) -> String {
+    #if SWIFT_PACKAGE && swift(>=5.3)
     let bundles: [Bundle] = [Bundle.main, Bundle.feedbackBundle, Bundle.module]
+    #else
+    let bundles: [Bundle] = [Bundle.main, Bundle.feedbackBundle]
+    #endif
+
     for bundle in bundles {
         let string = NSLocalizedString(key,
                                        tableName: "CTFeedbackLocalizable",
